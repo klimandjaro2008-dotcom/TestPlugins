a17f891f0ef037728e3ec09af275cd6fa5c1dcb0
klimandjaro2008-dotcom <klimandjaro2008@gmail.com>
2026-06-23 10:44:15 +0100

    Update ExampleProvider.kt

--- ExampleProvider/src/main/kotlin/com/example/ExampleProvider.kt
+++ ExampleProvider/src/main/kotlin/com/example/ExampleProvider.kt
@@ -8,7 +8,7 @@ class ExampleProvider : MainAPI() { // All providers must be an instance of Main
     override var mainUrl = "https://example.com/" 
     override var name = "Example provider"
     override val supportedTypes = setOf(TvType.Movie)
-
+    private val token = "PUT_YOUR_TOKEN"
     override var lang = "en"
 
     // Enable this when your provider has a main page
@@ -17,5 +17,56 @@ class ExampleProvider : MainAPI() { // All providers must be an instance of Main
     // This function gets called when you search for something
     override suspend fun search(query: String): List<SearchResponse> {
         return listOf()
+    }
+
+    
+
+    override suspend fun search(query: String): List<SearchResponse> {
+
+        val res = app.get(
+            "https://graph.microsoft.com/v1.0/me/drive/root/children",
+            headers = mapOf(
+                "Authorization" to "Bearer $token"
+            )
+        ).text
+
+        val items = parseJson(res)
+
+        return items.files.map {
+            newMovieSearchResponse(
+                it.name,
+                it.downloadUrl,
+                TvType.Movie
+            )
+        }
+    }
+
+    override suspend fun load(url: String): LoadResponse {
+        return newMovieLoadResponse(
+            name = "OneDrive Movie",
+            url = url,
+            type = TvType.Movie,
+            dataUrl = url
+        )
+    }
+
+    override suspend fun loadLinks(
+        data: String,
+        isCasting: Boolean,
+        subtitleCallback: (SubtitleFile) -> Unit,
+        callback: (ExtractorLink) -> Unit
+    ): Boolean {
+
+        callback(
+            ExtractorLink(
+                source = "OneDrive",
+                name = "HD",
+                url = data,
+                referer = "",
+                quality = Qualities.P1080.value,
+                isM3u8 = false
+            )
+        )
+        return true
     }
 }
